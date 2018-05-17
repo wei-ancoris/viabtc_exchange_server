bb2fed50db7b4a6b11aeb8c21c121d62c5ece9d8
Wei Zhang <sagamail@gmail.com>
2018-05-17 23:59:34 +0100

    Add quote for url

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /apps
-wget https://github.com/edenhill/librdkafka/archive/v0.11.0.tar.gz
-tar xfz v0.11.0.tar.gz
+wget "https://github.com/edenhill/librdkafka/archive/v0.11.0.tar.gz"
+tar xfz "v0.11.0.tar.gz"
 cd librdkafka-0.11.0 && ./configure && make -j 4 && make install
 cd /apps/depends/hiredis && make -j 4 && make install
 cd /apps/network && make -j 4
